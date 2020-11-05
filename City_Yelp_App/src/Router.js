3d0b5011d73d0102974b15059a5455fffefb98b9
serdar <serdar83durmus@gmail.com>
2020-11-05 16:28:10 +0100

    City Yelp App er oppdatert

--- City_Yelp_App/src/Router.js
+++ City_Yelp_App/src/Router.js
@@ -12,7 +12,7 @@ const Router = () => {
     return (
         <NavigationContainer>
             <Stack.Navigator>
-                <Stack.Screen name="Cityes" component={CityList} options={{ headerShown: false}}/>
+                <Stack.Screen name="Cities" component={CityList} options={{ headerShown: false}}/>
                 <Stack.Screen name="Restaurants" component={RestaurantList}  options={{ headerShown: false}}/>
                 <Stack.Screen name="Details" component={RestaurantDetail} options={{ headerShown: false}}/>
             </Stack.Navigator>
--- City_Yelp_App/src/pages/CityList.js
+++ City_Yelp_App/src/pages/CityList.js
@@ -1,11 +1,30 @@
-import React from 'react';
-import { SafeAreaView, Text, View, VirtualizedList} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { SafeAreaView, Text, View, FlatList} from 'react-native';
 
 const CityList = (props) => {
+    const [cityList, setCityList] = useState([]);
+    
+    // ASYNC - AWAIT
+    const fetchCityData = async () => {
+        const { data } = await axios.get("https://opentable.herokuapp.com/api/cities");
+        console.log(data);
+        setCityList(data.cities);
+    }
+
+    useEffect(() => {
+        fetchCityData();
+    }, [])
+
+    const renderCities = ({ item }) => <Text>{item}</Text>
     return (
         <SafeAreaView>
             <View>
-                <Text>CityList</Text>
+                <FlatList 
+                    keyExtractor= {(_, index) => index.toString()}
+                    data = {cityList}
+                    renderItem={renderCities}
+                />
             </View>
         </SafeAreaView>
     );
